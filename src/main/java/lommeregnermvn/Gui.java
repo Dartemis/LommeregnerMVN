48c75ae359c88343274bf2a4943d51cec6c13869
Dartemis <patrick.rorth@gmail.com>
2015-03-16 16:01:22 +0100

    Opdatering
    ny opsætning.
    
    Tilføjet knapper og nye funktioner

--- src/main/java/lommeregnermvn/Gui.java
+++ src/main/java/lommeregnermvn/Gui.java
@@ -1,14 +1,31 @@
 package lommeregnermvn;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.security.auth.Subject;
 import javax.swing.*;
 
-public class Gui extends JFrame {
-	JTextField showResult;
-	JButton[] button;
-	JButton regneregler;
+public class Gui extends JFrame implements ActionListener {
+	JTextField textfield;
+	
+	JButton button0;
+	JButton button1;
+	JButton button2;
+	JButton button3;
+	JButton button4;
+	JButton button5;
+	JButton button6;
+	JButton button7;
+	JButton button8;
+	JButton button9;
+	JButton buttonPlus;
+	JButton buttonMinus;
+	JButton buttonGange;
+	JButton buttonDividere;
+	JButton buttonClean;
+	
 	
 	public Gui( String title ){
 		super( title );
@@ -30,35 +47,49 @@ public class Gui extends JFrame {
 	
 	public void setGuiSize(){		
 		//textfeldt til resultat
-		showResult = new JTextField(40);
-		getContentPane().add(showResult);
+		textfield = new JTextField(40);
+		getContentPane().add(textfield);
 		
 		// Knapper fra 0-9
+		makeButtons();
 		setButtons();
+		addButtons();
 		
 		// knapper til + - * / =
-		regneregler = new JButton("+");
-		getContentPane().add(regneregler);
 		
-		regneregler = new JButton("-");
-		getContentPane().add(regneregler);
-		
-		regneregler = new JButton("x");
-		getContentPane().add(regneregler);
-		
-		regneregler = new JButton("/");
-		getContentPane().add(regneregler);
+	}
+	
+	public void makeButtons(){
+		button0 = new JButton();
+		button1 = new JButton();
+		button2 = new JButton();
+		button3 = new JButton();
+		button4 = new JButton();
+		button5 = new JButton();
+		button6 = new JButton();
+		button7 = new JButton();
+		button8 = new JButton();
+		button9 = new JButton();
+		buttonPlus = new JButton();
+		buttonMinus = new JButton();
+		buttonGange = new JButton();
+		buttonDividere = new JButton();
+		buttonClean = new JButton();
 	}
 	
 	public void setButtons(){
+		button0.setText("0");
 		
-		button = new JButton[10];
+	}
+	
+	public void addButtons(){
 		
-		for(int i=0; i < 10; i++){
-			button[i] = new JButton();
-			button[i].setText(Integer.toString(i));
-			getContentPane().add(button[i]);
-		}
+		
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 
